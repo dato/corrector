1d68fc35103e2f9f9da1957b321bfe1b457d1005
Dato Simó <dato@net.com.org.es>
2016-10-01 23:04:50 -0300

    Correr Moss por cuatrimestres separados, y sin padrones repetidos
    Para aliviar la detección de copias “todos contra todos”, correr contra
    cada cuatrimestre anterior por separado, y eliminando alumnos que
    cursaron de nuevo.

--- ojo_bionico.sh
+++ ojo_bionico.sh
@@ -2,13 +2,16 @@
 
 set -eu
 
+FIND="gfind"  # For macOS fiends.
+which $FIND >/dev/null 2>&1 || FIND="find"
+
 # TODO(dato): do not hardcode these.
 MOSS="/srv/fiuba/75.41/corrector/bin/moss"
 DATA="/srv/fiuba/75.41/corrector/data/tps"
 
 TP="${1-}"
 TP_DIR="$DATA/$TP"
-MOSS_TITLE="TDA $TP ($(date +%Y-%m-%d))"
+MOSS_TITLE="TDA $TP/$(date +%Y-%m-%d)"
 
 if [[ -z $TP ]]; then
   echo >&2 "Uso: $0 <NOMBRE_TP>"
@@ -20,13 +23,28 @@ if [[ ! -d "$TP_DIR" ]]; then
   exit 1
 fi
 
+TMP1=`mktemp`
+TMP2=`mktemp`
+
+trap "rm -f \"$TMP1\" \"$TMP2\"" EXIT
+
 # xargs’ -n 1000000000 makes it fail if it would split the files into
 # multiple moss invocations (which would defeate the point).
 #
 # `cd $TP_DIR` helps with the above by keeping the command line short.
 #
-cd "$TP_DIR" && find . -name '0*' -type d -prune -o  \
-     -type f -name '*.c' -not -name '__MAC*' -print0 |
-  xargs -0n 1000000000 $MOSS -d -l c -c "$MOSS_TITLE"
+CUR=$(ls "$TP_DIR" | sed -n '$p')
+PAST=$(ls -r "$TP_DIR" | sed '1d')
+
+for prev in $PAST; do
+    ls "$TP_DIR/$prev" >"$TMP1"
+    ls "$TP_DIR/$CUR"  >"$TMP2"
+    EXCLUDE=$(comm -12 "$TMP1" "$TMP2" | sed "s#^#$prev/#" | tr '\n' '|')
+
+    cd "$TP_DIR" && $FIND $CUR $prev -regextype egrep   \
+        \( -name '0*' -o -regex "$EXCLUDE" \) -prune -o \
+        -type f -name '*.c' -not -name '__MAC*' -print0 |
+    xargs -0n 1000000000 $MOSS -d -l c -c "$MOSS_TITLE (vs $prev)"
+done
 
 # TODO(dato): capture URL and post to Slack.
